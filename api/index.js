65c5028be71c8c80f58863c0b532b1ef7b03ced6
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2024-07-29 18:10:10 +0300

    created db instance

--- api/index.js
+++ api/index.js
@@ -1,1 +1,18 @@
-console.log("Hello World!");
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+const port = 3000;
+
+// console.log(process.env.MONGO)
+mongoose.connect(process.env.MONGO)
+.then(() => {
+    console.log('DB Connected Successfully!');
+    app.listen(port, () => {
+        console.log(`Server running on port: ${port}`);
+    })
+})
+.catch((err) => console.log(err));
