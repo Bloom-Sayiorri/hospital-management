447c68d7670a75b81e71a35acf937fa690ab4af0
Bloom-Sayiorri <bloomsayiorri@gmail.com>
2025-11-18 00:15:25 +0300

    notification model

--- api/src/models/notification.model.js
+++ api/src/models/notification.model.js
@@ -1,12 +1,22 @@
 const mongoose = require("mongoose");
 
 const notificationSchema = new mongoose.Schema(
-    {
-        
-    },
-    { timestamps: true }
+	{
+		patient: { type: mongoose.Schema.Types.ObjectId, ref: "Patient", required: true, },
+		title: { type: String, required: true },
+		message: { type: String, required: true },
+		appointment: { type: mongoose.Schema.Types.ObjectId, ref: "Appointment", required: true },
+		type: {
+			type: String,
+			enum: ["appointment", "billing", "test", "general"],
+			default: "general",
+            required: true,
+		},
+		isRead: { type: Boolean, default: false, required: true },
+	},
+	{ timestamps: true }
 );
 
-const Notification = mongoose.model('Notification', notificationSchema);
+const Notification = mongoose.model("Notification", notificationSchema);
 
 module.exports = Notification;
